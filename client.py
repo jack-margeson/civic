784a1db3eecc527d8cacb43352dd0e353f4ea6cd
Jack Margeson <margeson.jack@gmail.com>
2025-03-07 16:29:38 -0500

    Create client citizen management functions

--- client.py
+++ client.py
@@ -4,14 +4,12 @@
 # docker setup (create citizen) based on model
 
 import os
-import argparse
-import requests
 import docker
-import time
 import json
 from colorama import Fore, Style, init
 from enum import Enum
 import signal
+import prettytable
 
 VERSION = "v0.1.0"
 
@@ -26,25 +24,25 @@ menu_options = [
     ],
     [  # Main menu (1)
         {"key": "c", "command": "Manage Citizens", "status": 1},
-        {"key": "m", "command": "Manage Models", "status": 1},
     ],
     [  # Manage Citizens (2)
         {"key": "l", "command": "List Citizens", "status": 1},
         {"key": "c", "command": "Create Citizen", "status": 1},
+        {"key": "st", "command": "Start Citizen", "status": 1},
+        {"key": "sp", "command": "Stop Citizen", "status": 1},
         {"key": "d", "command": "Delete Citizen", "status": 1},
         {"key": "b", "command": "Back", "status": 1},
     ],
-    [  # Manage Models (3)
-        {"key": "l", "command": "List Models", "status": 1},
-        {"key": "d", "command": "Download Model", "status": 1},
-        {"key": "b", "command": "Back", "status": 1},
-    ],
 ]
-menu_states = Enum(
-    "Menu", ["GLOBAL", "MAIN", "MANAGE_CITIZENS", "MANAGE_MODELS"], start=0
-)
+menu_states = Enum("Menu", ["GLOBAL", "MAIN", "MANAGE_CITIZENS"], start=0)
 menu_state_titles = ["Global Commands", "Main Menu", "Manage Citizens", "Manage Models"]
 curr_menu = menu_states.MAIN
+
+# Get the Docker client
+docker_host = os.getenv("DOCKER_HOST_OVERRIDE", "unix://var/run/docker.sock")
+client = docker.DockerClient(base_url=docker_host)
+
+citizen_containers = []
 
 
 def main():
@@ -57,7 +55,7 @@ def main():
     print_menu(curr_menu)
 
     while True:
-        choice = input("$ ").lower()
+        choice = input("$ ").lower().strip()
 
         ### Global commands (0)
 
@@ -97,8 +95,6 @@ def main():
                 match choice:
                     case "c":
                         set_curr_menu(menu_states.MANAGE_CITIZENS)
-                    case "m":
-                        set_curr_menu(menu_states.MANAGE_MODELS)
             else:
                 if any(
                     choice == item["key"]
@@ -117,40 +113,21 @@ def main():
             ):
                 match choice:
                     case "l":
-                        print("Listing Citizens...\n")  # TODO
+                        list_citizens()
                     case "c":
-                        print("Creating Citizen...\n")  # TODO
+                        create_citizen()
                     case "d":
-                        print("Deleting Citizen...\n")  # TODO
+                        delete_citizen()
+                    case "st":
+                        start_citizen()
+                    case "sp":
+                        stop_citizen()
                     case "b":
                         set_curr_menu(menu_states.MAIN)
             else:
                 if any(
                     choice == item["key"]
                     for item in menu_options[menu_states.MANAGE_CITIZENS.value]
-                ):
-                    print_error("Command currently disabled. Please try again.")
-                else:
-                    print_error("Command not recognized. Please try again.")
-
-        ### Manage Models (3)
-
-        elif curr_menu == menu_states.MANAGE_MODELS:
-            if any(
-                choice == item["key"] and item["status"]
-                for item in menu_options[menu_states.MANAGE_MODELS.value]
-            ):
-                match choice:
-                    case "l":
-                        print("Listing Models...\n")
-                    case "d":
-                        print("Downloading Model...\n")
-                    case "b":
-                        set_curr_menu(menu_states.MAIN)
-            else:
-                if any(
-                    choice == item["key"]
-                    for item in menu_options[menu_states.MANAGE_MODELS.value]
                 ):
                     print_error("Command currently disabled. Please try again.")
                 else:
@@ -224,8 +201,161 @@ def print_error(message):
     print(Fore.RED + message + Style.RESET_ALL + "\n")
 
 
+def print_table(data):
+    headers = list(data[0].keys())
+    data.insert(0, headers)
+    table = prettytable.from_json(json.dumps(data))
+    print(table, "\n")
+
+
+def update_citizen_list():
+    global client, citizen_containers
+    citizen_containers = []
+    for container in client.containers.list(all=True):
+        if container.name.startswith("civic-internal-client"):
+            citizen_containers.append(container)
+
+
 def init_client():
-    print("Initializing client...")  # TODO
+    global client
+
+    print("Initializing client...")
+    # Build the client container if it doesn't exist
+    try:
+        client.images.get("civic-internal-client")
+    except docker.errors.ImageNotFound:
+        print("Building citizen image...")
+        client.images.build(
+            path="./internal_client",
+            dockerfile="Dockerfile",
+            tag="civic-internal-client:latest",
+        )
+
+    update_citizen_list()
+
+    print("Client initialized.")
+
+
+def create_citizen():
+    global client
+
+    ip = (
+        input("Enter the IP address of the server [default: localhost]: ")
+        or "localhost"
+    )
+    port = input("Enter the port of the server [default: 24842]: ") or "24842"
+    print(f"Creating citizen with server IP: {ip} and port: {port}")
+
+    # Get the highest citizen id
+    citizen_id = 1
+    for container in citizen_containers:
+        # Each citizen container has a unique id (starting from 1 and incrementing)
+        # Check the highest id and increment by 1
+        if int(container.name.split("-")[-1]) > len(citizen_containers):
+            citizen_id = int(container.name.split("-")[-1]) + 1
+        else:
+            citizen_id = len(citizen_containers) + 1
+
+    # Create citizen container
+    client.containers.run(
+        "civic-internal-client:latest",
+        name=f"civic-internal-client-{citizen_id}",
+        network="host",
+        detach=True,
+        environment={"CIVIC_SERVER_IP": ip, "CIVIC_SERVER_PORT": port},
+    )
+
+
+def list_citizens():
+    print("Listing citizens...")
+    # List all running containers
+    citizen_obj = []
+    for container in citizen_containers:
+        citizen_obj.append(
+            {
+                "id": container.name.split("-")[-1],
+                "name": container.name,
+                "status": container.status,
+            }
+        )
+    citizen_obj.sort(key=lambda x: x["id"])
+    print_table(citizen_obj)
+
+
+def delete_citizen():
+    list_citizens()
+    citizen_id = input("Enter the ID of the citizen to delete: ").strip()
+
+    # Find the container with the given ID
+    container_to_delete = None
+    for container in citizen_containers:
+        if container.name.endswith(f"-{citizen_id}"):
+            container_to_delete = container
+            break
+
+    if container_to_delete:
+        confirmation = (
+            input(f"Are you sure you want to delete citizen {citizen_id}? [y/N]: ")
+            .strip()
+            .lower()
+        )
+        if confirmation == "y":
+            print(f"Stopping and removing citizen {citizen_id}...")
+            container_to_delete.stop()
+            container_to_delete.remove()
+            citizen_containers.remove(container_to_delete)
+            print(f"Citizen {citizen_id} deleted.")
+            update_citizen_list()
+        else:
+            print("Deletion cancelled.")
+    else:
+        print_error("Citizen ID not found.")
+
+
+def start_citizen():
+    list_citizens()
+    citizen_id = input("Enter the ID of the citizen to start: ").strip()
+
+    # Find the container with the given ID
+    container_to_start = None
+    for container in citizen_containers:
+        if container.name.endswith(f"-{citizen_id}"):
+            container_to_start = container
+            break
+
+    if container_to_start:
+        if container_to_start.status == "exited":
+            print(f"Starting citizen {citizen_id}...")
+            container_to_start.start()
+            print(f"Citizen {citizen_id} started.")
+            update_citizen_list()
+        else:
+            print_error("Citizen is already running.")
+    else:
+        print_error("Citizen ID not found.")
+
+
+def stop_citizen():
+    list_citizens()
+    citizen_id = input("Enter the ID of the citizen to stop: ").strip()
+
+    # Find the container with the given ID
+    container_to_stop = None
+    for container in citizen_containers:
+        if container.name.endswith(f"-{citizen_id}"):
+            container_to_stop = container
+            break
+
+    if container_to_stop:
+        if container_to_stop.status == "running":
+            print(f"Stopping citizen {citizen_id}...")
+            container_to_stop.stop()
+            print(f"Citizen {citizen_id} stopped.")
+            update_citizen_list()
+        else:
+            print_error("Citizen is not running.")
+    else:
+        print_error("Citizen ID not found.")
 
 
 def safe_exit():
